1e7df070acb357d9b6663ed0572f42797cb03f91
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-12-21 21:58:33 +0200

    looger and actions
    added game variable in controller facade

--- Sudoku Verifiers/Sodoku_Verifier_1/src/Controller_Layer/Game.java
+++ Sudoku Verifiers/Sodoku_Verifier_1/src/Controller_Layer/Game.java
@@ -1,11 +1,16 @@
 package Controller_Layer;
 
+import Controller_Layer.Board.Board;
+import Controller_Layer.Board.BoardUtility;
 import Controller_Layer.CreatingBoards.DifficultyEnum;
+import Controller_Layer.Logging.LogActions;
 
 public class Game {
     public int[][] board; // Public as per existing code style
     private DifficultyEnum difficulty;
     private boolean isComplete;
+
+    private final String Game_File_PATH = "AppData/current/game.csv";
 
     public Game(int[][] board) {
         // IMPORTANT: DON'T COPY THE BOARD BY VALUE
@@ -40,5 +45,15 @@ public class Game {
 
     public int[][] getBoard() {
         return board;
+    }
+
+    public void addInput(int x, int y, int val) {
+        this.board[x][y] = val;
+        BoardUtility.saveBoard(board, Game_File_PATH);
+    }
+
+    public void endGame() {
+        BoardUtility.deleteFile(LogActions.LOG_FILE_PATH);
+
     }
 }
